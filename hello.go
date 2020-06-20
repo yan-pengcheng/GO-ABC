0945eda068d048b2f1a41b341b4552001b252c59
yan-pengcheng <gan_hotei@tre-inc.com>
2020-06-20 23:39:07 +0900

    add japanese test case

--- hello.go
+++ hello.go
@@ -4,16 +4,25 @@ import (
 	"fmt"
 )
 
+const spanish = "Spanish"
+const japanese = "japanese"
+const helloPrefix = "Hello, "
+const japaneseHelloPrefix = "ようこそ, "
 const englishHelloPrefix = "Hello, "
 
-//Hello print
-func Hello(name string) string {
+// Hello .
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == japanese {
+		return japaneseHelloPrefix + name
+	}
+
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -12,14 +12,20 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in japanese", func(t *testing.T) {
+		got := Hello("佐藤", "japanese")
+		want := "ようこそ, 佐藤"
 		assertCorrectMessage(t, got, want)
 	})
 
