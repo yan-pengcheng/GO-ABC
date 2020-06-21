2b3b9af9e248818d57b2323d2a77501f9e299c3e
yan-pengcheng <gan_hotei@tre-inc.com>
2020-06-21 23:30:44 +0900

    add sun_test.go

---
+++ iteration/for_test.go
@@ -0,0 +1,28 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected '%q' but got '%q'", expected, repeated)
+	}
+}
+
+const repeatCount = 5
+
+func Repeat(character string) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
---
+++ sum_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	got := Sum(numbers)
+	want := 15
+
+	if want != got {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
+}
+func Sum(numbers [5]int) int {
+	sum := 0
+	for i := 0; i < 5; i++ {
+		sum += numbers[i]
+	}
+	return sum
+}
